d12825815aa58af2ad4fcf1bf9bfa05709c2f275
jenkdarr <jenkdarr@gmail.com>
2022-03-06 01:44:41 -0700

    edit code

---
+++ src/week2/material/JavaWeek2.java
@@ -0,0 +1,10 @@
+package week2.material;
+
+public class JavaWeek2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Trying to learn more about github.");
+	}
+
+}
