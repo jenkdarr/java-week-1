9fca9377496198c49e1841cfc02b33496aa66de6
jenkdarr <jenkdarr@gmail.com>
2022-03-07 02:16:49 -0700

    edit code

--- src/week2/material/JavaWeek2.java
+++ src/week2/material/JavaWeek2.java
@@ -6,6 +6,21 @@ public class JavaWeek2 {
 		// TODO Auto-generated method stub
       System.out.println("Trying to learn more about github and the computer directory");
       
+      //Boolean operators
+      
+      // == equality operator
+      System.out.println(3 == 3);//True
+      System.out.println(3 > 5);//False
+      System.out.println(3 <= 5);//True
+      System.out.println(5 <= 5);//False
+      System.out.println("Separate code groups.");
+      // && and   || or
+      System.out.println(true && true);//True
+      System.out.println(true && false);//False
+      System.out.println(true || false);//True
+      System.out.println(false || false );//False
+      
+
 	}
 
 }
